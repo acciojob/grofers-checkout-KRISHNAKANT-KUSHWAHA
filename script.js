9131f2f4644016d6464fa88bd35ba2469e6079ef
Krishnakant Kushwaha <145730098+KRISHNAKANT-KUSHWAHA@users.noreply.github.com>
2026-07-02 01:12:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,22 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	const prices=document.querySelectorAll(".price");
+	// sum
+   let sum=0;
+	prices.forEach((ele)=>{
+		sum+=parseInt(ele.textContent.trim());
+	});
+	// new row
+	const table=document.querySelector("table");
+	const newRow=document.createElement("tr");
+	const newCell=document.createElement("td");
+	// data in the cell
+	newCell.setAttribute("colspan","2");
+	newCell.textContent=`Total Price: Rs ${sum}`;
+	//
+	newRow.appendChild(newCell);
+    table.appendChild(newRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
